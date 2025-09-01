e67011252176b86b51c1f402effb1294f817a7d0
enmvnuel <cristopherariasp@icloud.com>
2025-09-01 12:35:08 -0500

    Refactor App component to render Landing page and reorganize CSS imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
+import Landing from "./pages/Landing.jsx";
 function App() {
-    return (
-        <>
-            <h1 className="text-8xl font-semibold flex h-screen items-center justify-center">
-                Hello world!
-            </h1>
-        </>
-    );
+    return <Landing />;
 }
 
 export default App;
